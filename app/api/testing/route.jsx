ae657743c410dc8b55b5f00ad1a6a42992e1cb40
Kamit6337 <kamit6337@gmail.com>
2024-03-03 02:04:41 +0530

    remove route handler

--- app/api/testing/route.jsx
+++
@@ -1,9 +0,0 @@
-import Req from "@utils/server/Req";
-
-export const GET = async (request) => {
-  const { params, cookies } = Req(request);
-
-  const { cookie_1 } = cookies;
-
-  return Response.json({ params, cookies, cookie_1 });
-};
