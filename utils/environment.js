f51a4dc0e05aef174b41764ab3e9a8724e2d4a59
Kamit6337 <kamit6337@gmail.com>
2024-12-16 14:33:45 +0530

    minor update to environment

--- utils/environment.js
+++ utils/environment.js
@@ -1,6 +1,6 @@
 const environment = {
   SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
-  NODE_ENV: process.env.NODE_ENV,
+  NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
 };
 
 export default environment;
